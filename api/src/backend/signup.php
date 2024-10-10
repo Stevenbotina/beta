e670ab78007af75e75e0157d384a5a49c393adba
Stevenn <sdgelpud.1656@unicesmag.edu.co>
2024-10-10 11:54:17 -0500

    Login form

--- api/src/backend/signup.php
+++ api/src/backend/signup.php
@@ -9,12 +9,24 @@
     //Encrypt password using md5 algorithm
     $enc_pass = md5($pass);
 
+    //Validate if email already exists
+    $query = "SELECT * FROM users WHERE email = '$email'";
+    $result = pg_query($conn, $query);
+    $row = pg_fetch_assoc($result);
+    
+    if ($row) {
+        echo "<script>alert('Email already exists!')</script>";
+        header('Refresh:0; url=http://127.0.0.1/beta/api/src/register_form.html');
+        exit();
+    }
+
     //Insert data into database
     $query = "INSERT INTO users (email, password) VALUES ('$email', '$enc_pass')";
     $result = pg_query($conn, $query);
 
     if ($result) {
-        echo "User registered successfully";
+        echo "<script>alert('Registration Successful!')</script>";
+        header('Refresh:0; url=http://127.0.0.1/beta/api/src/login_form.html');
     } else {
         echo "Error occurred ";
     }
